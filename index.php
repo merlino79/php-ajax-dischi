742fa8ba2d5ddc075fc8ba973efe1087d84d7d50
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-08 18:03:49 +0200

    in lavorazione

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
  <?php
-    require_once __DIR__ . '/data/dba.php';
+    require_once __DIR__ . '/data/db.php';
 ?> 
 
 
@@ -9,9 +9,27 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="asset/css/style.css">
   <title>Document</title>
 </head>
 <body>
+<header>
+
+<div class="logo">
+<img src="asset/img/logo.jpg" alt="">
+</div>
+
+<div class="generi">
+ <select name="" id="generi">
+       <option value=""><srong>rock</<strong></option>
+            <option value=""><srong>metal</<strong></option>
+            <option value=""><srong>classic</<strong></option>      
+ </select>
+
+</div>
+
+</header>
+
 
 <?php 
 
@@ -19,12 +37,12 @@
 ?>
 
 <ul>
-  <?php foreach($database as $disco){ ?>
+  <?php foreach($response as $disco){ ?>
 
     <li>
       <?php echo $disco['title'] ?>
       <?php echo $disco['author'] ?>
-      <?php echo $disco['poster'] ?>
+      <img src="<?php echo $disco['poster'] ?>" alt="">
     </li>
 
   <?php } ?>
