cc002331cb6682059ed94e07cd17feb2d9fbf30f
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-09 16:05:38 +0200

    risistemazoine chiamata axios nel methods

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,27 +1,31 @@
 const app = new Vue({
+
     el: '#app',
     data: {
 
         dischi: [],
-        mess: 'errore nell\'url',
+        apiURL: 'http://localhost:8888/php-ajax-dischi/data/api.php'
+    },
 
-
-
-
+    methods: {
+        getAPI() {
+            axios.get(this.apiURL, )
+                .then((res) => {
+                    this.dischi = res.data;
+                    console.log(res.data);
+                })
+                //per vedere errori nelle chiamtae api        
+                .catch((err) => {
+                    console.log(err);
+                })
+        }
 
 
     },
-    mounted() {
-        axios.get('http://localhost:8888/php-ajax-dischi/data/api.php')
-            .then((res) => {
-                this.dischi = res.data;
-                console.log(res.data);
-            })
-            //per vedere errori nelle chiamtae api        
-            .catch((err) => {
-                console.log(err);
-            })
-    },
+
+    created() {
+        this.getAPI();
+    }
 
 });
 
